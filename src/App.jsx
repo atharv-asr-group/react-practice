1a523ce20e18dcd0794c49a5240ed9cf9d527f5d
unknown <atharv21240@iiitd.ac.in>
2024-01-04 11:17:22 +0530

    example of setctr creating infinite loop, need to use useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -3,42 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-var todos=[{
-  title:"go to gym",
-  description:"hi",
-  id:1
-},{
-  title:"go to gym fe",
-  description:"hi",
-  id:1
-},{
-  title:"go to gymd wd",
-  description:"hi",
-  id:1
-},{
-  title:"go to gym hello",
-  description:"hi",
-  id:1
-}]
 
 function App() {
-  // const [todo,setTodo]=useState(todos);
-  // setInterval(()=>{
-  //   setTodo({
-  //     title:"go to ",
-  //     description:"hi",
-  //     id:1
-  //   })
-    
-  // }, 2000);
+  const [ctr,setctr]=useState(1);
+  console.log('render');
+    setctr(5);
+
   return (
     <>
+      {ctr}
       <h1>hi there</h1>
-      {todos.map((value)=>{
-        return(
-          <Todo title={value.title} description={value.description}/>
-        )
-      })}
+      
     </>
   )
 }
