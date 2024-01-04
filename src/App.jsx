797628d082e440b85e37a91644104fec0f378857
unknown <atharv21240@iiitd.ac.in>
2024-01-04 10:57:43 +0530

    maps for arrays

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,53 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-var todos={
+var todos=[{
   title:"go to gym",
   description:"hi",
   id:1
-}
+},{
+  title:"go to gym fe",
+  description:"hi",
+  id:1
+},{
+  title:"go to gymd wd",
+  description:"hi",
+  id:1
+},{
+  title:"go to gym hello",
+  description:"hi",
+  id:1
+}]
 
 function App() {
-  const [todo,setTodo]=useState(todos);
-  setInterval(()=>{
-    setTodo({
-      title:"go to ",
-      description:"hi",
-      id:1
-    })
+  // const [todo,setTodo]=useState(todos);
+  // setInterval(()=>{
+  //   setTodo({
+  //     title:"go to ",
+  //     description:"hi",
+  //     id:1
+  //   })
     
-  }, 2000);
+  // }, 2000);
   return (
     <>
       <h1>hi there</h1>
-      {todo.title}
-      <Footer firstname={"Atharv"} lastname={"Srivastava"}/>
+      {todos.map((value)=>{
+        return(
+          <Todo title={value.title} description={value.description}/>
+        )
+      })}
     </>
   )
 }
-function Footer(props){
-  return(
-    <div>
-      {props.firstname} 
-      {props.lastname}
-    </div>
-  )
+function Todo(value){
+return(
+  <div>
+            {value.title}
+            {value.description}
+          </div>
+)
 }
+
 
 export default App
